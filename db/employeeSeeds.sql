4cfac869a27f4412bdd8c7f0d66417bcd79d6bae
Nathan <119708133+nathantr97@users.noreply.github.com>
2023-03-27 14:48:45 -0700

    added department, role and employees

---
+++ db/employeeSeeds.sql
@@ -0,0 +1,24 @@
+USE company;
+
+INSERT INTO department (name)
+VALUES
+("Management"),
+("Sales"),
+("Merchandise"),
+("HR");
+
+INSERT INTO roles (title, salary, department_id)
+VALUES
+("Store Manager", 100000, 1),
+("Sales Lead", 80000, 2),
+("Stock Lead", 50000, 3),
+("Legal Team", 70000, 4);
+
+INSERT INTO employees (first_name, last_name, role_id)
+VALUES
+("Nick", "Johnson", 1),
+("Jira", "Hilton", 2),
+("Yuna", "Lee", 3),
+("Jessica", "Lu", 4)
+("Jonas", "Brien", 2)
+("Frank", "Zhang", 4);
